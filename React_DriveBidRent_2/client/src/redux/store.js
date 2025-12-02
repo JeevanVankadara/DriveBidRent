b3f938905cd0414f9acdfce85e8f0d09343fd13b
Tejesh2005 <madalatejesh90@gmail.com>
2025-12-03 03:31:37 +0530

    wishlist redux

--- React_DriveBidRent_2/client/src/redux/store.js
+++ React_DriveBidRent_2/client/src/redux/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+
 import authReducer from './slices/authSlice';
 import profileReducer from './slices/profileSlice';
+import wishlistReducer from './slices/wishlistSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     profile: profileReducer,
+    wishlist: wishlistReducer,
   }
 });
 
