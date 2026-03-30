a608cdf18a6f5290537be82a07066fdcd611f39c
Jeevan116 <jeevanv1997@gmail.com>
2026-03-30 16:04:03 +0530

    removed the extra files

--- DrivrBidRent/routes/Mechanic/pending-tasks.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const isMechanicLoggedin = require('../../middlewares/isMechanicLoggedin');
-
-router.get("/pending-tasks", isMechanicLoggedin, (req, res) => {
-  res.render("mechanic_dashboard/pending-tasks.ejs");
-});
-
-module.exports = router;
