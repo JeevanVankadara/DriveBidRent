9b8246015fdafb7d39dc9a0cf5636548fc7f5880
JeevanVankadara <jeevanv1997@gmail.com>
2026-04-20 22:10:27 +0530

    removed error

--- React_DriveBidRent_2/client/src/App.jsx
+++ React_DriveBidRent_2/client/src/App.jsx
@@ -2,7 +2,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 // === PUBLIC ROUTES ===
-import HomePage from './pages/HomePage';
+import HomePage from './pages/auth/HomePage';
 import Login from './pages/auth/Login';
 import Signup from './pages/auth/Signup';
 import AuctionManagerSignup from './pages/auth/auctionManagersignup';
